e9091bf9ff10322ef7d5bb21fa4ac373a8f7cc4b
Jacob Poulsgaard Tjørnholm <jacob@tjoernholm.dk>
2017-12-17 17:46:05 +0100

    Remove elpa

--- .emacs.d/elpa/ag-0.42/ag-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ag" "0.42" "A front-end for ag ('the silver searcher'), the C ack replacement." 'nil)
--- .emacs.d/elpa/git-commit-20170821.2057/git-commit-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "git-commit" "20170821.2057" "Edit Git commit messages" '((emacs "24.4") (dash "20170207.2056") (with-editor "20170111.609")) :commit "3e51dbc32382dc472e375650976fd8207c2db8ab" :url "https://github.com/magit/magit" :keywords '("git" "tools" "vc"))
