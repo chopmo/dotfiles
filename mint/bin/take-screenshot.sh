1bf0ea1c95f5938f756a1e2b9d9e56c909190a00
Jacob Poulsgaard Tjørnholm <jacob@tjoernholm.dk>
2017-12-17 17:47:16 +0100

    Fix typo in screenshot script

--- mint/bin/take-screenshot.sh
+++ mint/bin/take-screenshot.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-g/usr/bin/import /tmp/last-screenshot.png && xclip -selection clip -t image/png /tmp/last-screenshot.png
+/usr/bin/import /tmp/last-screenshot.png && xclip -selection clip -t image/png /tmp/last-screenshot.png
