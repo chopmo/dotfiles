04afc996b8c71334b3e2f6224755f981e0c4b177
Jacob Poulsgaard Tjørnholm <jacob@tjoernholm.dk>
2018-01-07 22:15:48 +0100

    A bit more i3 config, especially about monitors

--- mint/bin/init-monitors.rb
+++ mint/bin/init-monitors.rb
@@ -6,10 +6,12 @@
 # Notify with notify-send (maybe?)
 # cat /proc/acpi/button/lid/LID/state
 
-if `xrandr`[/HDMI1 connected/]
-  `xrandr --output HDMI1 --primary --mode 2560x1440`
+# Dual monitor setup: xrandr --output eDP1 --left-of DP1-1 --mode 1920x1080 --output DP1-1 --primary --mode 2560x1440
+
+if `xrandr`[/DP1-1 connected/]
+  `xrandr --output DP1-1 --primary --mode 2560x1440`
   `xrandr --output eDP1 --off`
-  `notify-send "Selected HDMI1"`
+  `notify-send "Selected DP1-1"`
 else
   `xrandr --auto`
   `notify-send "Selected auto"`
