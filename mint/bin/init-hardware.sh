59bf8b3291cd8dcdb584340bb208687882cfd2cf
Jacob Poulsgaard Tjørnholm <jacob@tjoernholm.dk>
2018-07-05 09:43:41 +0200

    Move touchpad init to hardware init + steelseries support

--- mint/bin/init-hardware.sh
+++ mint/bin/init-hardware.sh
@@ -5,3 +5,8 @@ BASEDIR=$(dirname $0)
 
 $BASEDIR/init-keyboards.sh
 ruby $BASEDIR/init-monitors.rb
+
+# Enable stereo output for SteelSeries headset
+pacmd load-module module-alsa-sink device=hw:1,1
+
+synclient VertScrollDelta=-114 MinSpeed=1.50 MaxSpeed=2.50 TapButton1=0 TapButton2=0
