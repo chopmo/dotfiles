33920524e250a2fdbd6296e8d0da384e7158d6fc
Jacob Poulsgaard Tjørnholm <jacob@tjoernholm.dk>
2018-08-11 17:17:52 +0200

    Update primitive ruby script for initialising monitors
    This is such a hack. I should read up on how to automate the setup of
    monitors to my liking when docking and undocking.

--- mint/bin/init-monitors.rb
+++ mint/bin/init-monitors.rb
@@ -9,12 +9,14 @@
 # Dual monitor setup: xrandr --output eDP1 --left-of DP1-1 --mode 1920x1080 --output DP1-1 --primary --mode 2560x1440
 
 if `xrandr`[/DP1-1 connected/]
-  #`xrandr --output DP1-1 --primary --mode 2560x1440`
   `xrandr --output DP1-1 --primary --mode 3440x1440`
-  #`xrandr --output DP1-2 --primary --mode 3440x1440`
   `xrandr --output eDP1 --off`
   `notify-send "Selected DP1-1"`
+elsif `xrandr`[/DP1-2 connected/]
+  `xrandr --output DP1-2 --primary --mode 3440x1440`
+  `xrandr --output eDP1 --off`
+  `notify-send "Selected DP1-2"`
 else
-  `xrandr --auto`
-  `notify-send "Selected auto"`
+  `xrandr --output eDP1 --primary --mode 1920x1080`
+  `notify-send "Selected eDP1"`
 end
