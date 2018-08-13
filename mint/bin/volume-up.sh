b814b5641e1cb85641d3a2dadf492bbbf46b3ef3
Jacob Poulsgaard Tjørnholm <jacob@tjoernholm.dk>
2018-08-13 14:00:50 +0200

    Update keybindings to adjust volume
    Still very primitive, but I can't be bothered to spend time finding a
    better solution now.

---
+++ mint/bin/volume-up.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# So primitive, but works
+amixer -c 0 sset PCM 2dB+
+amixer -c 1 sset PCM 2dB+
+amixer -c 2 sset PCM 2dB+
+amixer -c 3 sset PCM 2dB+
+amixer -c 4 sset PCM 2dB+
