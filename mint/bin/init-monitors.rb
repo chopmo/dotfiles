76ee6398b56c194650c5ef256e6ea9c5980b97f5
Jacob Poulsgaard Tjørnholm <jacob@tjoernholm.dk>
2018-08-18 14:33:06 +0200

    Bulk commit

--- mint/bin/init-monitors.rb
+++ mint/bin/init-monitors.rb
@@ -8,15 +8,12 @@
 
 # Dual monitor setup: xrandr --output eDP1 --left-of DP1-1 --mode 1920x1080 --output DP1-1 --primary --mode 2560x1440
 
-if `xrandr`[/DP1-1 connected/]
-  `xrandr --output DP1-1 --primary --mode 3440x1440`
+if `xrandr`[/HDMI1 connected/]
+  `xrandr --output HDMI1 --primary --mode 3440x1440`
   `xrandr --output eDP1 --off`
-  `notify-send "Selected DP1-1"`
-elsif `xrandr`[/DP1-2 connected/]
-  `xrandr --output DP1-2 --primary --mode 3440x1440`
-  `xrandr --output eDP1 --off`
-  `notify-send "Selected DP1-2"`
+  `notify-send "Selected HDMI1"`
 else
   `xrandr --output eDP1 --primary --mode 1920x1080`
+  `xrandr --output HDMI1 --off`
   `notify-send "Selected eDP1"`
 end
