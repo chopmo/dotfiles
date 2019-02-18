80793277e421ee6349c7061d13d08ec9ce49aa47
Jacob Poulsgaard Tjørnholm <jacob@tjoernholm.dk>
2019-02-18 09:47:45 +0100

    Remove osx specific code
    Don't think I'm going back to Mac anytime soon.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -82,21 +82,9 @@
     yasnippet
     ))
 
-;; On OS X, an Emacs instance started from the graphical user
-;; interface will have a different environment than a shell in a
-;; terminal window, because OS X does not run a shell during the
-;; login. Obviously this will lead to unexpected results when
-;; calling external utilities like make from Emacs.
-;; This library works around this problem by copying important
-;; environment variables from the user's shell.
-;; https://github.com/purcell/exec-path-from-shell
-(if (eq system-type 'darwin)
-    (add-to-list 'my-packages 'exec-path-from-shell))
-
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
-
 
 ;; Place downloaded elisp files in ~/.emacs.d/vendor. You'll then be able
 ;; to load them.
