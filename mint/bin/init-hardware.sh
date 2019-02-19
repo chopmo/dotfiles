e94d1bd1e723f25bb5c9a9563764cefbf93558d9
Jacob Poulsgaard Tjørnholm <jacob@tjoernholm.dk>
2019-02-19 06:27:36 +0100

    For some reason, synclient is working again
    Maybe I didn't test after rebooting..?

--- mint/bin/init-hardware.sh
+++ mint/bin/init-hardware.sh
@@ -9,14 +9,13 @@ ruby $BASEDIR/init-monitors.rb
 # Enable stereo output for SteelSeries headset
 pacmd load-module module-alsa-sink device=hw:1,1
 
-# synclient VertScrollDelta=-114 MinSpeed=1.50 MaxSpeed=2.50 TapButton1=0 TapButton2=0
-
+synclient VertScrollDelta=-114 MinSpeed=1.50 MaxSpeed=2.50 TapButton1=0 TapButton2=0
 
 # Enable natural scrolling
 #
 # Source https://github.com/maxwell-bland/i3-natural-scrolling-and-tap/blob/master/inverse-scroll.sh
-id=`xinput list | grep "Synaptic" | cut -d'=' -f2 | cut -d'[' -f1`
-natural_scrolling_id=`xinput list-props $id | \
-                      grep "Natural Scrolling Enabled (" \
-                      | cut -d'(' -f2 | cut -d')' -f1`
-xinput --set-prop $id $natural_scrolling_id 1
+# id=`xinput list | grep "Synaptic" | cut -d'=' -f2 | cut -d'[' -f1`
+# natural_scrolling_id=`xinput list-props $id | \
+#                       grep "Natural Scrolling Enabled (" \
+#                       | cut -d'(' -f2 | cut -d')' -f1`
+# xinput --set-prop $id $natural_scrolling_id 1
