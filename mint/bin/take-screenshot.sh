e08eab0e35bbc0c104b4a1b50c4da15133e7c46a
Jacob Poulsgaard Tjørnholm <jacob@tjoernholm.dk>
2019-11-26 07:46:02 +0100

    Store last screenshot in downloads

--- mint/bin/take-screenshot.sh
+++ mint/bin/take-screenshot.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-/usr/bin/import /tmp/last-screenshot.png && xclip -selection clip -t image/png /tmp/last-screenshot.png
+/usr/bin/import /home/jtj/Downloads/last-screenshot.png && xclip -selection clip -t image/png /home/jtj/Downloads/last-screenshot.png
