9d80c8706763556215b497b3874b9f532b8459ff
Jacob Poulsgaard Tjørnholm <jacob@tjoernholm.dk>
2019-11-26 07:42:43 +0100

    Swap ctrl and caps instead of both being ctrl
    Because I miss having a caps lock for writing SQL.

--- mint/bin/dk-keyboard.sh
+++ mint/bin/dk-keyboard.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-setxkbmap -option -option ctrl:nocaps,grp_led:scroll dk
+setxkbmap -option -option ctrl:swapcaps,grp_led:scroll dk
 xmodmap -e "keycode 107 = Super_L"
 xset r rate 250 40
--- mint/bin/us-keyboard.sh
+++ mint/bin/us-keyboard.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-setxkbmap -option -option ctrl:nocaps,grp_led:scroll us
+setxkbmap -option -option ctrl:swapcaps,grp_led:scroll us
 xmodmap -e "keycode 107 = Super_L"
 xset r rate 250 40
