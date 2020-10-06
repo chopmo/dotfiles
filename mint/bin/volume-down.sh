1e507e80b5eb7d1697096de426cd4bcecfa9f622
Jacob Poulsgaard Tjørnholm <jacob@tjoernholm.dk>
2020-10-06 21:39:34 +0200

    vol by 5%

--- mint/bin/volume-down.sh
+++ mint/bin/volume-down.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # So primitive, but works
-pactl set-sink-volume 1 -10%
-pactl set-sink-volume 2 -10%
-pactl set-sink-volume 3 -10%
-pactl set-sink-volume 4 -10%
+pactl set-sink-volume 1 -5%
+pactl set-sink-volume 2 -5%
+pactl set-sink-volume 3 -5%
+pactl set-sink-volume 4 -5%
