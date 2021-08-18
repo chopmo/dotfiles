0ab140e78df23d747493a38fac2cb31f759ed33b
Jacob Poulsgaard Tjørnholm <jacob@tjoernholm.dk>
2021-08-18 08:32:04 +0200

    Disable rainbow delimiters in mhtml-mode
    They don't really work.

--- .emacs.d/customizations/setup-html.el
+++ .emacs.d/customizations/setup-html.el
@@ -1,2 +1,6 @@
 (add-hook 'html-mode-hook (lambda ()
                             (tagedit-mode 0)))
+
+(add-hook 'mhtml-mode-hook (lambda ()
+                             (tagedit-mode 0)
+                             (rainbow-delimiters-mode-disable)))
