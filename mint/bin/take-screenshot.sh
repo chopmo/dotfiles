6c2e1e6d1e24a645a2da47044bde60579856d6e9
Jacob Poulsgaard Tjørnholm <jacob@tjoernholm.dk>
2021-11-11 08:51:45 +0100

    Use flameshot to take SS

--- mint/bin/take-screenshot.sh
+++ mint/bin/take-screenshot.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-/usr/bin/import /home/jtj/Downloads/last-screenshot.png && xclip -selection clip -t image/png /home/jtj/Downloads/last-screenshot.png
+# /usr/bin/import /home/jtj/Downloads/last-screenshot.png && xclip -selection clip -t image/png /home/jtj/Downloads/last-screenshot.png
+
+flameshot gui
