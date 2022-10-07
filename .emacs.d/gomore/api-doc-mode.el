c531b9f525e000e1652124117c91551e236d4d05
Jacob Poulsgaard Tjørnholm <jacob@tjoernholm.dk>
2022-10-07 15:05:39 +0200

    Add mode to hide api doc boilerplate

---
+++ .emacs.d/gomore/api-doc-mode.el
@@ -0,0 +1,43 @@
+(require 'hide-lines)
+
+(setq jpt-api-doc-mode-hiding nil)
+
+(make-variable-buffer-local 'jpt-api-doc-mode-hiding)
+
+(defun jpt-api-doc-mode-hide ()
+  (let ((keywords '("android"
+                    "application/json"
+                    "content"
+                    "description"
+                    "format"
+                    "introduced"
+                    "ios"
+                    "nullable"
+                    "operationId"
+                    "platform"
+                    "produces"
+                    "properties"
+                    "responses"
+                    "schema"
+                    "status"
+                    "summary"
+                    "tags"
+                    "type")))
+    (hide-lines-matching (mapconcat (lambda (s) (concat "\\b" s "\\b")) keywords  "\\|"))))
+
+(defun jpt-api-doc-mode-toggle ()
+  (interactive)
+  (setq jpt-api-doc-mode-hiding (not jpt-api-doc-mode-hiding))
+  (if jpt-api-doc-mode-hiding
+      (jpt-api-doc-mode-hide)
+      (hide-lines-show-all)))
+
+(define-minor-mode jpt-api-doc-mode
+  "Toggles global API doc mode"
+  nil   ; Initial value, nil for disabled
+  :global t
+  :group 'dotfiles
+  :keymap (let ((map (make-sparse-keymap)))
+            (define-key map (kbd "C-M-h") 'jpt-api-doc-mode-toggle)
+            map)
+  :lighter " API-doc")
