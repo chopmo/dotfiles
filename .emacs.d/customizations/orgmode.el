4fce428caeb6f299bd0cfa1b9c5e4baed2dac90f
Jacob Poulsgaard Tjørnholm <jacob@tjoernholm.dk>
2023-02-17 16:04:56 +0100

    Encrypt org journal

--- .emacs.d/customizations/orgmode.el
+++ .emacs.d/customizations/orgmode.el
@@ -2,3 +2,19 @@
 (require 'org-journal)
 
 (setq org-journal-dir "~/Dropbox/org/journal")
+
+(use-package epa-file
+  :ensure nil
+  :config
+  (setq epa-file-encrypt-to '("jacob@tjoernholm.dk"))
+  :custom
+  (epa-file-select-keys 'silent))
+
+(use-package org-crypt
+  :ensure nil  ;; included with org-mode
+  :after org
+  :config
+  (org-crypt-use-before-save-magic)
+  (setq org-tags-exclude-from-inheritance (quote ("crypt")))
+  :custom
+  (org-crypt-key "jacob@tjoernholm.dk"))
