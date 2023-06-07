fd0c312a6eb88bb20b589baacdd8c066722281e5
Jacob Poulsgaard Tjørnholm <jacob@tjoernholm.dk>
2023-06-07 14:04:57 +0200

    add org-roam

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -213,3 +213,18 @@
 
 ;; Enable repeated mark pops with C-u C-SPC C-SPC C-SPC etc...
 (setq set-mark-command-repeat-pop t)
+
+(use-package org-roam
+  :ensure t
+  :init
+  (setq org-roam-v2-ack t)
+  :custom
+  (org-roam-directory "~/Dropbox/org-roam")
+  (org-roam-completion-everywhere t)
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n i" . org-roam-node-insert)
+         :map org-mode-map
+         ("C-M-i"    . completion-at-point))
+  :config
+  (org-roam-setup))
