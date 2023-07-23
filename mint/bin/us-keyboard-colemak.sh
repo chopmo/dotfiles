6aea54754d7506902fcb2daf5b9f570b88a3e0d1
Jacob Poulsgaard Tjørnholm <jacob@tjoernholm.dk>
2023-07-23 19:49:19 +0200

    Colemak experiment
    I'm still not sure I want to give this a proper try...

---
+++ mint/bin/us-keyboard-colemak.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+setxkbmap -option -option ctrl:swapcaps us -variant colemak
+
+# TODO: What was this?
+#
+
+# Map (in order):
+# - caps lock key to control
+# - left ctrl key to Mode_switch
+# - Semicolon key to support ae with Mode_switch
+# - Apostrophe key to support oslash with Mode_switch
+# - Bracketleft key to support aring with Mode_switch
+# - PrtSc on Thinkpad to Super L
+xmodmap -e "keycode 66 = Control_L NoSymbol Control_L" \
+        -e "keycode 37 = Mode_switch NoSymbol Mode_switch" \
+        -e "keycode 47 = o O ae AE" \
+        -e "keycode 48 = apostrophe quotedbl oslash Oslash" \
+        -e "keycode 34 = bracketleft braceleft aring Aring" \
+        -e "keycode 107 = Super_L" \
+
+
+
+xset r rate 200 40
