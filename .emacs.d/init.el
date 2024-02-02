d0cf77f3f54edd68838fee3942802c66a418ae43
Jacob Poulsgaard Tjørnholm <jacob@tjoernholm.dk>
2024-02-02 15:00:39 +0100

    get rid of misc.el because of a conflict

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -155,9 +155,6 @@
 ;; These customizations make editing a bit nicer.
 (load "editing.el")
 
-;; Hard-to-categorize customizations
-(load "misc.el")
-
 ;; For editing lisps
 (load "elisp-editing.el")
 
@@ -233,3 +230,44 @@
 
 ;; Enable repeated mark pops with C-u C-SPC C-SPC C-SPC etc...
 (setq set-mark-command-repeat-pop t)
+
+;; Changes all yes/no questions to y/n type
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; shell scripts
+(setq-default sh-basic-offset 2)
+(setq-default sh-indentation 2)
+
+;; No need for ~ files when editing
+(setq create-lockfiles nil)
+
+;; Go straight to scratch buffer on startup
+(setq inhibit-startup-message t)
+
+(setq ag-reuse-buffers t)
+
+;; These functions require the ddcctl utility to be installed.
+;; Clone this repo and run `make; make install` in it: https://github.com/kfix/ddcctl
+(defun jpt-monitor-brightness-up (monitor-id)
+  (async-shell-command (concat "ddcctl -d " (number-to-string monitor-id) " -b 10+")))
+
+(defun jpt-monitor-brightness-down (monitor-id)
+  (async-shell-command (concat "ddcctl -d " (number-to-string monitor-id) " -b 10-")))
+
+(defun jpt-isearch-forward-word-at-point ()
+  (interactive)
+  (isearch-forward-word nil 1)
+  (let ((bounds (find-tag-default-bounds)))
+    (cond
+     (bounds
+      (when (< (car bounds) (point))
+	(goto-char (car bounds)))
+      (isearch-yank-string
+       (buffer-substring-no-properties (car bounds) (cdr bounds))))
+     (t
+      (setq isearch-error "No symbol at point")
+      (isearch-push-state)
+      (isearch-update)))))
+
+;; Default dired to copying/moving to another window
+(setq dired-dwim-target t)
