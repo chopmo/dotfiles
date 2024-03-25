a808e4270632b46d8d8eb23dda7e72cf18f60a42
Jacob Poulsgaard Tjørnholm <jacob@tjoernholm.dk>
2024-03-25 14:23:14 +0100

    rename old dir

---
+++ .emacs.d.old/transient/history.el
@@ -0,0 +1,16 @@
+((magit-branch nil)
+ (magit-commit nil)
+ (magit-diff
+  ("--no-ext-diff" "--stat"))
+ (magit-fetch nil)
+ (magit-file-dispatch nil)
+ (magit-gitignore nil)
+ (magit-merge nil)
+ (magit-pull nil)
+ (magit-push nil
+             ("--force-with-lease"))
+ (magit-rebase
+  ("--autostash")
+  ("--autostash" "--interactive")
+  nil)
+ (magit-stash nil))
