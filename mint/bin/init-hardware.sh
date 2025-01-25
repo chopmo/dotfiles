37f466467eb09ae240b8d6090f24d842e5a88ae3
Jacob Poulsgaard Tjørnholm <jacob@tjoernholm.dk>
2025-01-26 00:24:13 +0100

    set f23 key permanently

--- mint/bin/init-hardware.sh
+++ mint/bin/init-hardware.sh
@@ -3,6 +3,11 @@ set -x
 
 BASEDIR=$(dirname $0)
 
+# Map copilot scancode to F23 to allow me to remap it with keyd
+# Requires this line in /etc/keyd/default.conf:
+# leftshift+leftmeta+f23 = overload(meta, noop)
+/usr/bin/setkeycodes 6e 193
+
 $BASEDIR/us-keyboard.sh
 ruby $BASEDIR/init-monitors.rb
 
