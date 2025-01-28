7628508081e13a4ecaccb84762e1eb5787c94d3a
Jacob Poulsgaard Tjørnholm <jacob@tjoernholm.dk>
2025-01-28 23:42:01 +0100

    remove defunct libinput accel speed

--- mint/bin/init-hardware.sh
+++ mint/bin/init-hardware.sh
@@ -16,4 +16,3 @@ pacmd load-module module-alsa-sink device=hw:1,1
 
 synclient VertScrollDelta=-114 MinSpeed=1.50 MaxSpeed=2.50 TapButton1=0 TapButton2=0
 
-xinput --set-prop 9 "libinput Accel Speed" 0.5
