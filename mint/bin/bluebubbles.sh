46970c4358462ee38c7e82745e473bd5608cf929
Jacob Poulsgaard Tjørnholm <jacob@tjoernholm.dk>
2025-03-25 07:26:17 +0100

    Apparently it's the same scale for Bluebubbles

--- mint/bin/bluebubbles.sh
+++ mint/bin/bluebubbles.sh
@@ -1,6 +1,2 @@
 #!/usr/bin/env bash
-if [ `hostname` = "bau8" ]; then
-  GDK_SCALE=2 flatpak run app.bluebubbles.BlueBubbles
-else
-  GDK_SCALE=1 flatpak run app.bluebubbles.BlueBubbles
-fi
+GDK_SCALE=2 flatpak run app.bluebubbles.BlueBubbles
