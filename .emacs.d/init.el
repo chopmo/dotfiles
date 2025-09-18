c288a3cf1edadd4e42aac81953cf5e652f0f73f7
Jacob Poulsgaard Tjørnholm <jacob@tjoernholm.dk>
2025-09-18 10:56:10 +0200

    Explicitly enable prettier mode for js files
    I'm not sure why this is needed.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -473,6 +473,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (add-to-list 'auto-mode-alist '("\\.js$" . js-mode))
 (add-hook 'js-mode-hook 'subword-mode)
+(add-hook 'js-mode-hook 'prettier-mode)
 (add-hook 'html-mode-hook 'subword-mode)
 (setq js-indent-level 2)
 
