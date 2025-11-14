ea2c0395945d6deaa885925110c7928bce25a56e
Jacob Poulsgaard Tjørnholm <jacob@tjoernholm.dk>
2025-11-14 11:14:04 +0100

    modern way to request short answers

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -743,7 +743,7 @@
 (setq set-mark-command-repeat-pop t)
 
 ;; Changes all yes/no questions to y/n type
-(fset 'yes-or-no-p 'y-or-n-p)
+(setq use-short-answers t)
 
 ;; shell scripts
 (setq-default sh-basic-offset 2)
